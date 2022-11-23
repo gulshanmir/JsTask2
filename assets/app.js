e58fcfbbddc88f22f692179a41ffeb0f6e8b905d
gulshanmir <gulshansm.code.edu.az>
2022-11-24 03:44:30 +0400

    first commit

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    console.log("Answer the following question")
+}
+function myFunction2(){
+    console.log("Yaşınızı daxil edin")
+}
+function Qrup(){
+    let x=document.getElementById("inp").value;
+    console.log(x) 
+}
+
+   
+function Qrup(){
+    let x=document.getElementById("inp").value;
+    console.log (x.trim())
+}
+    
+function Qrup(){
+    let x=document.getElementById("inp").value;
+    let a=x.slice(-3)
+    console.log(x.slice(-3))
+
+    if(a=1){
+        document.body.style.backgroundColor="yellow";
+    }
+    else if(a=2){
+        document.body.style.backgroundColor="red";
+    }
+     else if(a=3){
+        document.body.style.backgroundColor="black";
+    }
+    
+
+}
